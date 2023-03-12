6fe80ea41f90dcf995e5d5c265a5fb3ee3450aae
Kai Six <petersen.codie@gmail.com>
2023-03-11 22:22:58 -0800

    Removed comments.

--- sidechain/xrp/commands.py
+++ sidechain/xrp/commands.py
@@ -7,12 +7,10 @@ def ping_command(id="ping"):
     return json.dumps(command)
 
 def subscribe_accounts(id="subscribe", accounts=[]):
-    #["rrpNnNLKrartuEqfJGpqyDwPj1AFPg9vn1"]
     subscribe = { "id": id, "command": "subscribe", "accounts": accounts }
     return json.dumps(subscribe)
 
 def subscribe_stream(id="subscribe", streams=["transactions_proposed"]):
-    #["rrpNnNLKrartuEqfJGpqyDwPj1AFPg9vn1"]
     subscribe = { "id": id, "command": "subscribe", "streams":streams }
     return json.dumps(subscribe)
 
